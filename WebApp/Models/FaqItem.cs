b94521295c3b46a9550ca743c3317ade32bd31fb
Zaw Myo Hein <30889380+zawmyohein1@users.noreply.github.com>
2025-09-05 17:33:50 +0800

    Add FAQ page

---
+++ WebApp/Models/FaqItem.cs
@@ -0,0 +1,8 @@
+namespace WebApp.Models;
+
+public class FaqItem
+{
+    public string Question { get; set; } = string.Empty;
+    public string Answer { get; set; } = string.Empty;
+}
+
--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -9,21 +9,17 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
